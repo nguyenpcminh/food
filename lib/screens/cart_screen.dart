28f97c29c78d46203323fe5656028f146a758e40
Nguyen Minh <minh.npc.0607@gmail.com>
2020-05-22 13:10:27 +0700

    final cart

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -17,67 +17,100 @@ class _CartScreenState extends State<CartScreen> {
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          Row(
-            children: <Widget>[
-              Container(
-                width: 150,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage(order.food.imageUrl),
-                    fit: BoxFit.cover,
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  width: 150,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(order.food.imageUrl),
+                      fit: BoxFit.cover,
+                    ),
+                    borderRadius: BorderRadius.circular(15.0),
                   ),
-                  borderRadius: BorderRadius.circular(15.0),
                 ),
-              ),
-              Expanded(
-                child: Container(
-                  margin: EdgeInsets.all(10.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        order.food.name,
-                        style: TextStyle(
-                            fontSize: 24.0, fontWeight: FontWeight.bold),
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      Text(
-                        order.restaurant.name,
-                        style: TextStyle(
-                            fontSize: 20.0, fontWeight: FontWeight.w600),
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      Row(
-                        children: <Widget>[
-                          InkWell(
-                            onTap: () {},
-                            child: Text(
-                              '-',
-                              style: TextStyle(
-                                fontSize: 20,
-                                color: Theme.of(context).primaryColor,
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(10.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          order.food.name,
+                          style: TextStyle(
+                              fontSize: 24.0, fontWeight: FontWeight.bold),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          order.restaurant.name,
+                          style: TextStyle(
+                              fontSize: 20.0, fontWeight: FontWeight.w600),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        SizedBox(height: 10),
+                        Container(
+                          width: 100,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(30),
+                              border: Border.all(
+                                width: .7,
+                                color: Colors.black54,
+                              )),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: <Widget>[
+                              InkWell(
+                                onTap: () {
+                                  print('-');
+                                },
+                                child: Text(
+                                  '-',
+                                  style: TextStyle(
+                                    fontSize: 24,
+                                    color: Theme.of(context).primaryColor,
+                                  ),
+                                ),
                               ),
-                            ),
+                              Text(
+                                '${order.quantity}',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.black87,
+                                ),
+                              ),
+                              InkWell(
+                                onTap: () {},
+                                child: Text(
+                                  '+',
+                                  style: TextStyle(
+                                    fontSize: 24,
+                                    color: Theme.of(context).primaryColor,
+                                  ),
+                                ),
+                              )
+                            ],
                           ),
-                          Text('${order.quantity * order.food.price}'),
-                          InkWell(
-                            onTap: () {},
-                            child: Text(
-                              '+',
-                              style: TextStyle(
-                                fontSize: 20,
-                                color: Theme.of(context).primaryColor,
-                              ),
-                            ),
-                          )
-                        ],
-                      )
-                    ],
+                        )
+                      ],
+                    ),
                   ),
                 ),
-              )
-            ],
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Text(
+                    '\$${order.quantity * order.food.price}',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              ],
+            ),
           )
         ],
       ),
@@ -86,6 +119,10 @@ class _CartScreenState extends State<CartScreen> {
 
   @override
   Widget build(BuildContext context) {
+    double totalPrice = 0;
+    Provider.of<FoodData>(context).currentUser.cart.forEach((element) {
+      totalPrice += element.quantity * element.food.price;
+    });
     return Consumer<FoodData>(
       builder: (context, foodData, child) {
         return Scaffold(
@@ -94,15 +131,77 @@ class _CartScreenState extends State<CartScreen> {
             title: Text('Cart (${foodData.currentUser.cart.length})'),
           ),
           body: ListView.separated(
+            itemCount: foodData.currentUser.cart.length + 1,
             itemBuilder: (context, int index) {
-              Order order = foodData.currentUser.cart[index];
-              return _buildCartItem(order);
+              if (index < foodData.currentUser.cart.length) {
+                Order order = foodData.currentUser.cart[index];
+                return _buildCartItem(order);
+              }
+              return Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(
+                          'Estimated Delivery Time:',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.w700),
+                        ),
+                        Text(
+                          '25 min',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.w400),
+                        )
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(
+                          'Total Cost:',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.w700),
+                        ),
+                        Text(
+                          '\$${totalPrice.toStringAsFixed(2)}',
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.green),
+                        )
+                      ],
+                    ),
+                    SizedBox(height:100),
+                  ],
+                ),
+              );
             },
             separatorBuilder: (context, int index) => Divider(
               height: 1.0,
               color: Colors.grey,
             ),
-            itemCount: foodData.currentUser.cart.length,
+          ),
+          bottomSheet: Container(
+            width: MediaQuery.of(context).size.width,
+            height: 100,
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(30),
+                topLeft: Radius.circular(30),
+              ),
+            ),
+            child: Center(
+              child: Text(
+                'CHECKOUT',
+                style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w600,
+                    letterSpacing: 2),
+              ),
+            ),
           ),
         );
       },
